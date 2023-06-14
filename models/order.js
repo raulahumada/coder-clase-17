0d9a42a0e60084d7da69f92f13b11ade9fb7e604
rahumada <raulahumada00@gmail.com>
2023-06-13 21:08:47 -0300

    Entrega

---
+++ models/order.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+const orderCollection = "orders";
+
+const orderSchema = mongoose.Schema({
+  name: String,
+  size: {
+    type: String,
+    enum: ["small", "medium", "large"],
+    default: "medium",
+  },
+  price: Number,
+  quantity: Number,
+  date: Date,
+});
+
+orderSchema.plugin(mongoosePaginate);
+
+const orderModel = mongoose.model(orderCollection, orderSchema);
+
+export default orderModel;
